d6ce0b19c748caded4e781adbcbc5434a9e57054
Jonathan R. Warden <john.warden@gmail.com>
2025-07-27 17:02:24 -0600

    Update lexicon: note=>proposal, rating=>vote

--- src/lib/api/community-notes.ts
+++ src/lib/api/community-notes.ts
@@ -1,10 +1,23 @@
 import {type BskyAgent} from '@atproto/api'
 import {sha256} from 'js-sha256'
 
+type VoteValue = 'helpful' | 'somewhat_helpful' | 'not_helpful'
+
+function mapVoteValue(value: VoteValue): 1 | 0 | -1 {
+  switch (value) {
+    case 'helpful':
+      return 1
+    case 'somewhat_helpful':
+      return 0
+    case 'not_helpful':
+      return -1
+  }
+}
+
 export async function createNoteRating(
   agent: BskyAgent,
   note: {uri: string; cid?: string},
-  value: 'helpful' | 'somewhat_helpful' | 'not_helpful',
+  value: VoteValue,
   reasons: string[],
 ) {
   if (!agent.session) {
@@ -13,15 +26,13 @@ export async function createNoteRating(
   const aid = 'org.opencommunitynotes:' + sha256.hex(agent.session.did)
 
   const response = await agent.api.com.atproto.repo.createRecord({
-    collection: 'org.opencommunitynotes.rating',
+    collection: 'org.opencommunitynotes.vote',
     repo: agent.session.did,
     record: {
-      $type: 'org.opencommunitynotes.rating',
-      subject: {
-        uri: note.uri,
-        ...(note.cid && {cid: note.cid}),
-      },
-      val: value,
+      $type: 'org.opencommunitynotes.vote',
+      uri: note.uri,
+      ...(note.cid && {cid: note.cid}),
+      val: mapVoteValue(value),
       reasons: reasons,
       aid: aid,
       createdAt: new Date().toISOString(),
@@ -34,7 +45,7 @@ export async function updateNoteRating(
   agent: BskyAgent,
   ratingUri: string,
   note: {uri: string; cid?: string},
-  value: 'helpful' | 'somewhat_helpful' | 'not_helpful',
+  value: VoteValue,
   reasons: string[],
 ) {
   if (!agent.session) {
@@ -47,16 +58,14 @@ export async function updateNoteRating(
   }
 
   return agent.api.com.atproto.repo.putRecord({
-    collection: 'org.opencommunitynotes.rating',
+    collection: 'org.opencommunitynotes.vote',
     repo: agent.session.did,
     rkey: rkey,
     record: {
-      $type: 'org.opencommunitynotes.rating',
-      subject: {
-        uri: note.uri,
-        ...(note.cid && {cid: note.cid}),
-      },
-      val: value,
+      $type: 'org.opencommunitynotes.vote',
+      uri: note.uri,
+      ...(note.cid && {cid: note.cid}),
+      val: mapVoteValue(value),
       reasons: reasons,
       aid: aid,
       createdAt: new Date().toISOString(),
@@ -74,7 +83,7 @@ export async function deleteNoteRating(agent: BskyAgent, ratingUri: string) {
   }
 
   return agent.api.com.atproto.repo.deleteRecord({
-    collection: 'org.opencommunitynotes.rating',
+    collection: 'org.opencommunitynotes.vote',
     repo: agent.session.did,
     rkey: rkey,
   })
