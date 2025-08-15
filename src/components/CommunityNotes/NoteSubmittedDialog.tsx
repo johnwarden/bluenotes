bcb7357e4de29cfd58d87852e87a9ca8599fe827
Jonathan R. Warden <john.warden@gmail.com>
2025-08-15 15:56:27 -0600

    streamline note submission dialog with separate note details
    - Replace cluttered note details section with clean two-button layout
    - Add 'Note Details' button that opens dedicated details dialog
    - Maintain 'Done' button for quick dismissal
    - Improve user experience by separating concerns

--- src/components/CommunityNotes/NoteSubmittedDialog.tsx
+++ src/components/CommunityNotes/NoteSubmittedDialog.tsx
@@ -2,132 +2,122 @@ import {View} from 'react-native'
 import {msg, Trans} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
+import {type CommunityNote} from '#/lib/mock-data/community-notes'
 import {atoms as a, useTheme} from '#/alf'
 import {Button, ButtonText} from '#/components/Button'
+import {NoteDetailsDialog} from '#/components/CommunityNotes/NoteDetailsDialog'
 import * as Dialog from '#/components/Dialog'
 import {Text} from '#/components/Typography'
 
 interface NoteSubmittedDialogProps {
   control: Dialog.DialogOuterProps['control']
-  onSeeNote?: () => void
   noteUri?: string
+  note?: CommunityNote
+  onRefresh?: () => void
 }
 
 export function NoteSubmittedDialog({
   control,
-  onSeeNote,
-  noteUri,
+  noteUri: _noteUri,
+  note,
+  onRefresh,
 }: NoteSubmittedDialogProps) {
   const t = useTheme()
   const {_} = useLingui()
-
-  const handleSeeNote = () => {
-    onSeeNote?.()
-    control.close()
-  }
+  const noteDetailsControl = Dialog.useDialogControl()
 
   const handleDone = () => {
     control.close()
+    onRefresh?.()
+  }
+
+  const handleClose = () => {
+    // Don't call control.close() here as it creates infinite recursion
+    // This function is called BY the dialog close, not to close the dialog
+    onRefresh?.()
+  }
+
+  const handleShowDetails = () => {
+    control.close()
+    noteDetailsControl.open()
   }
 
   return (
-    <Dialog.Outer control={control}>
-      <Dialog.Handle />
-      <Dialog.ScrollableInner label={_(msg`Note submitted`)}>
-        <Dialog.Close />
-        <View style={[a.px_2xl, a.py_2xl]}>
-          {/* Main Heading */}
-          <Text style={[a.text_2xl, a.font_bold, a.mb_xl, t.atoms.text]}>
-            <Trans>Your note is submitted!</Trans>
-          </Text>
-
-          {/* Subheading */}
-          <Text style={[a.text_md, a.font_bold, a.mb_lg, t.atoms.text]}>
-            <Trans>Other contributors can now rate your note</Trans>
-          </Text>
-
-          {/* Debug Section - Show Note URI */}
-          {noteUri && (
-            <View
-              style={[
-                a.mb_lg,
-                a.p_md,
-                a.rounded_md,
-                {backgroundColor: t.palette.contrast_50},
-              ]}>
-              <Text style={[a.text_sm, a.font_bold, a.mb_xs, t.atoms.text]}>
-                Debug - Note URI:
-              </Text>
-              <Text
-                style={[
-                  a.text_xs,
-                  {fontFamily: 'monospace'},
-                  t.atoms.text_contrast_medium,
-                ]}>
-                {noteUri}
-              </Text>
-            </View>
-          )}
-
-          {/* Information Sections */}
-          <View style={[a.mb_2xl]}>
-            <Text style={[a.text_md, a.font_bold, a.mb_lg, t.atoms.text]}>
-              <Trans>
-                If your note earns a status of Helpful, it will start showing as
-                context to everyone who sees the post
-              </Trans>
+    <>
+      <Dialog.Outer control={control} onClose={handleClose}>
+        <Dialog.Handle />
+        <Dialog.ScrollableInner label={_(msg`Note submitted`)}>
+          <Dialog.Close />
+          <View style={[a.px_2xl, a.py_2xl]}>
+            {/* Main Heading */}
+            <Text style={[a.text_2xl, a.font_bold, a.mb_xl, t.atoms.text]}>
+              <Trans>Your note is submitted!</Trans>
             </Text>
 
-            <View>
-              <Text style={[a.text_md, a.font_bold, a.mb_sm, t.atoms.text]}>
-                <Trans>Note statuses aren't defined by majority rule</Trans>
-              </Text>
-              <Text style={[a.text_md, t.atoms.text_contrast_medium]}>
+            {/* Subheading */}
+            <Text style={[a.text_md, a.font_bold, a.mb_lg, t.atoms.text]}>
+              <Trans>Other contributors can now rate your note</Trans>
+            </Text>
+
+            {/* Information Sections */}
+            <View style={[a.mb_2xl]}>
+              <Text style={[a.text_md, a.font_bold, a.mb_lg, t.atoms.text]}>
                 <Trans>
-                  To identify notes that are helpful to a wide range of people,
-                  note statuses require agreement between contributors who have
-                  sometimes disagreed in their past ratings.
+                  If your note earns a status of Helpful, it will start showing
+                  as context to everyone who sees the post
                 </Trans>
               </Text>
+
+              <View>
+                <Text style={[a.text_md, a.font_bold, a.mb_sm, t.atoms.text]}>
+                  <Trans>Note statuses aren't defined by majority rule</Trans>
+                </Text>
+                <Text style={[a.text_md, t.atoms.text_contrast_medium]}>
+                  <Trans>
+                    To identify notes that are helpful to a wide range of
+                    people, note statuses require agreement between contributors
+                    who have sometimes disagreed in their past ratings.
+                  </Trans>
+                </Text>
+              </View>
+            </View>
+
+            {/* Action Buttons */}
+            <View style={[a.flex_row, a.gap_md]}>
+              <Button
+                variant="outline"
+                color="secondary"
+                label={_(msg`Note Details`)}
+                style={[
+                  a.flex_1,
+                  {
+                    borderRadius: 20,
+                    paddingVertical: 12,
+                  },
+                ]}
+                onPress={handleShowDetails}>
+                <ButtonText>Note Details</ButtonText>
+              </Button>
+              <Button
+                variant="outline"
+                color="secondary"
+                label={_(msg`Done`)}
+                style={[
+                  a.flex_1,
+                  {
+                    borderRadius: 20,
+                    paddingVertical: 12,
+                  },
+                ]}
+                onPress={handleDone}>
+                <ButtonText>Done</ButtonText>
+              </Button>
             </View>
           </View>
+        </Dialog.ScrollableInner>
+      </Dialog.Outer>
 
-          {/* Action Buttons */}
-          <View style={[a.gap_md]}>
-            {/* Primary Button - See your note */}
-            <Button
-              variant="outline"
-              color="primary"
-              label={_(msg`See your note`)}
-              style={[
-                a.w_full,
-                {
-                  borderRadius: 20,
-                  paddingVertical: 12,
-                },
-              ]}
-              onPress={handleSeeNote}>
-              <ButtonText>See your note</ButtonText>
-            </Button>
-
-            {/* Secondary Button - Done */}
-            <Button
-              variant="outline"
-              color="secondary"
-              label={_(msg`Done`)}
-              style={[
-                a.w_full,
-                {
-                  borderRadius: 20,
-                  paddingVertical: 12,
-                },
-              ]}
-              onPress={handleDone}>
-              <ButtonText>Done</ButtonText>
-            </Button>
-          </View>
-        </View>
-      </Dialog.ScrollableInner>
-    </Dialog.Outer>
+      {note && <NoteDetailsDialog control={noteDetailsControl} note={note} />}
+    </>
   )
 }
