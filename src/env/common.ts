19c00cb4bc936559eec407773a31a58d40e28988
Jonathan R. Warden <john.warden@gmail.com>
2025-10-21 19:43:03 +0200

    refactor: use configurable APP_NAME in Community Notes

--- src/env/common.ts
+++ src/env/common.ts
@@ -1,12 +1,14 @@
+import Constants from 'expo-constants'
 import {type Did} from '@atproto/api'
 
 import packageJson from '#/../package.json'
 
 /**
- * The name of the app. Defaults to 'Bluesky' but can be overridden
- * (e.g., to 'Bluenotes') via the EXPO_PUBLIC_APP_NAME environment variable.
+ * The name of the app, as defined in `app.config.js`.
+ * This will automatically reflect the app name (e.g., 'Bluesky' or 'Bluenotes')
+ * based on what's configured in the Expo config.
  */
-export const APP_NAME: string = process.env.EXPO_PUBLIC_APP_NAME || 'Bluesky'
+export const APP_NAME: string = Constants.expoConfig?.name || 'Bluesky'
 
 /**
  * The semver version of the app, as defined in `package.json.`
